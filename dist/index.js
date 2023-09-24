7a966f65b20e735f04450a4343aac64fb2ddd02a
Vladyslav <dr.tienam123@gmail.com>
2023-09-24 03:27:09 +0300

    Add something

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+container.addEventListener("click", onClick);
+console.log(container);
+function onClick(event) {
+  event
+  console.dir(e.target.dataset.color);
+}
